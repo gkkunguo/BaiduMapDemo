536e4ecd61ffb1a33a4bbd32f1052c34049e39de
gkkunguo <gkkunguo@163.com>
2021-01-11 10:01:16 +0800

    Initial commit

---
+++ app/src/main/java/com/hk/baidumapdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hk.baidumapdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = this.findViewById(R.id.mMapView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
