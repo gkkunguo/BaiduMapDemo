eab1b27f41f6bfcd07b67dea06370163948263f6
gkkunguo <gkkunguo@163.com>
2021-01-11 15:59:38 +0800

    修改文件

--- app/src/main/java/com/hk/baidumapdemo/MainActivityDemo1.java
+++ app/src/main/java/com/hk/baidumapdemo/MainActivityDemo1.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.baidu.mapapi.map.MapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityDemo1 extends AppCompatActivity {
     private MapView mMapView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
