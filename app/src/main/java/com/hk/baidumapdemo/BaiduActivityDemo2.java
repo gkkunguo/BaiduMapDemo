d2ecf571bd4b8861ddc11223ac83f39ece53d884
gkkunguo <gkkunguo@163.com>
2021-02-26 16:45:53 +0800

    提交修改代码

--- app/src/main/java/com/hk/baidumapdemo/BaiduActivityDemo2.java
+++ app/src/main/java/com/hk/baidumapdemo/BaiduActivityDemo2.java
@@ -2,7 +2,9 @@ package com.hk.baidumapdemo;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,6 +24,9 @@ import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.InfoWindow;
 import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
@@ -30,11 +35,12 @@ import com.baidu.mapapi.map.Overlay;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.model.LatLng;
-import com.baidu.mapapi.utils.CoordinateConverter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 
 public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.OnMarkerClickListener, BaiduMap.OnMapClickListener {
     private MapView mMapView;
@@ -43,6 +49,13 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
     private final static String MAP_CONTENT = "MAP_CONTENT";
     private View showInfoWindow;
     private List<LatLng> points = new ArrayList<>();//轨迹经纬度
+    private static final String TAG = "BaiduActivityDemo2";
+    public static final int MODEL_PATROL_START = 23;
+    private static final float MAPZOOM_DEFAULT = 13f;
+    private static final float MAPZOOM_TRACK = 17f;
+    private BitmapDescriptor mBlueBitmap = BitmapDescriptorFactory.fromAsset("icon_road_blue_arrow.png");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +111,7 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
      * 初始化定位参数配置
      */
     private void initLocationOption() {
+        mBaiduMap.setMyLocationEnabled(true);
         //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
         LocationClient locationClient = new LocationClient(getApplicationContext());
         //声明LocationClient类实例并配置定位参数
@@ -108,7 +122,7 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
         //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
         //可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-        locationOption.setCoorType("gcj02");
+        locationOption.setCoorType("bd09ll");
         //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
         locationOption.setScanSpan(2000);
         //可选，设置是否需要地址信息，默认不需要
@@ -159,23 +173,25 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
     public class MyLocationListener extends BDAbstractLocationListener {
         @Override
         public void onReceiveLocation(BDLocation location){
-            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-            //以下只列举部分获取经纬度相关（常用）的结果信息
-            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            if(location != null){
+                //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+                //以下只列举部分获取经纬度相关（常用）的结果信息
+                //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
 
-            //获取纬度信息
-            double latitude = location.getLatitude();
-            //获取经度信息
-            double longitude = location.getLongitude();
-            //获取定位精度，默认值为0.0f
-            float radius = location.getRadius();
-            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
-            String coorType = location.getCoorType();
-            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-            int errorCode = location.getLocType();
+                //获取纬度信息
+                double latitude = location.getLatitude();
+                //获取经度信息
+                double longitude = location.getLongitude();
+                //获取定位精度，默认值为0.0f
+                float radius = location.getRadius();
+                //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+                String coorType = location.getCoorType();
+                //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+                int errorCode = location.getLocType();
 
-            LatLng latLng = new LatLng( latitude,longitude);
-            drawLine(location,latLng);
+                LatLng latLng = new LatLng( latitude,longitude);
+                drawLine(location,latLng);
+            }
 
         }
     }
@@ -183,27 +199,17 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
 
     private void drawLine(BDLocation location,LatLng latLng){
         Toast.makeText(this,"latLng="+latLng.latitude,Toast.LENGTH_LONG).show();
+        Log.i(TAG, "drawLine: "+points.toString());
+//        mBaiduMap.clear();
+        mMapView.invalidate();
         points.add(latLng);
-        // 标注当前位置
-        CoordinateConverter converts = new CoordinateConverter();
-        converts.coord(latLng);
-        converts.from(CoordinateConverter.CoordType.COMMON);
-        LatLng curLatLng = converts.convert();
-        MyLocationData mylocData = new MyLocationData.Builder()
-                .accuracy(location.getRadius())
-                .direction(location.getDirection())
-                .speed(location.getSpeed())
-                .latitude(curLatLng.latitude)
-                .longitude(curLatLng.longitude)
-                .build();
-        mBaiduMap.setMyLocationData(mylocData);
 
         //开始位置
         BitmapDescriptor bitmapDescriptorStart = BitmapDescriptorFactory.fromResource(R.drawable.map_lvl_ld_start);
         OverlayOptions optionStart = new MarkerOptions()
                 .position(points.get(0))//标注位置
                 .icon(bitmapDescriptorStart)//图标
-                .animateType(MarkerOptions.MarkerAnimateType.jump)//动画类型
+                .animateType(MarkerOptions.MarkerAnimateType.none)//动画类型
                 .perspective(true)//是否开启近大远小效果
                 .zIndex(900)//设置覆盖物的zIndex
                 ;
@@ -217,24 +223,39 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
         if(points.size() >= 2 ){
             //轨迹过程中的线
             OverlayOptions mOverlayOptions = new PolylineOptions()
-                    .width(5)//折线宽度
-                    .color(0xAAFF0000)//折线颜色
-                    .dottedLine(true)
+                    .width(12)//折线宽度
+                    .color(Color.argb(255, 255, 0, 0))//折线颜色
+                    .dottedLine(true)////设置折线显示为虚线
+                    .customTexture(mBlueBitmap)//设置路径图片
                     .points(points);
             Overlay mPolyline = mBaiduMap.addOverlay(mOverlayOptions);
 
-            //当前位置
-            BitmapDescriptor bitmapDescriptorNow = BitmapDescriptorFactory.fromResource(R.drawable.map_lvl_ld_loc);
-            OverlayOptions optionNow = new MarkerOptions()
-                    .position(points.get(points.size()-1))//标注位置 取得最后一个点数据
-                    .icon(bitmapDescriptorNow)//图标
-                    .animateType(MarkerOptions.MarkerAnimateType.jump)//动画类型
-                    .perspective(true)//是否开启近大远小效果
-                    .zIndex(900)//设置覆盖物的zIndex
-                    ;
-
-            Overlay overlayNow = mBaiduMap.addOverlay(optionNow);
         }
+
+        //当前位置
+       /* BitmapDescriptor bitmapDescriptorNow = BitmapDescriptorFactory.fromResource(R.drawable.map_lvl_ld_loc);
+        OverlayOptions optionNow = new MarkerOptions()
+                .position(points.get(points.size()-1))//标注位置 取得最后一个点数据
+                .icon(bitmapDescriptorNow)//图标
+                .animateType(MarkerOptions.MarkerAnimateType.none)//动画类型
+                .perspective(true)//是否开启近大远小效果
+                .zIndex(900)//设置覆盖物的zIndex
+                ;
+        Overlay overlayNow = mBaiduMap.addOverlay(optionNow);*/
+
+
+        MyLocationData locData = new MyLocationData.Builder()
+                .accuracy(location.getRadius())
+                // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(location.getDirection())
+                .latitude(location.getLatitude())
+                .longitude(location.getLongitude())
+                .build();
+        mBaiduMap.setMyLocationData(locData);
+
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(new MapStatus.Builder().target(latLng).zoom(MODEL_PATROL_START).build());
+        mBaiduMap.animateMapStatus(mapStatusUpdate);
+
 
     }
 
@@ -251,7 +272,7 @@ public class BaiduActivityDemo2 extends AppCompatActivity implements BaiduMap.On
 
         //该marker所在位置
         LatLng latLng = marker.getPosition();
-        InfoWindow infoWindow = new InfoWindow(showInfoWindow,latLng,-30);
+        InfoWindow infoWindow = new InfoWindow(showInfoWindow,latLng,0);
         mBaiduMap.showInfoWindow(infoWindow);
 
         return true;//若响应点击事件，返回true，否则返回false
